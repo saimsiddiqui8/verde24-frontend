d82082f82c3de1c7a5dbf7caee809ab02833028a
Ruona Ogechi Izuagbala <danwilly178@yahoo.com>
2024-05-04 13:21:38 +0200

    Added small fixes (#2)

--- src/api/requestMethods.ts
+++ src/api/requestMethods.ts
@@ -6,7 +6,10 @@ const user = data ? JSON.parse(data)?.user : null;
 const currentUser = JSON.parse(user)?.currentUser;
 const TOKEN = currentUser?.token;
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `https://dev-api.verde24health.com`,
+  headers: {
+    "Content-Type": "application/json",
+  }
 });
 
 export const userRequest = axios.create({
