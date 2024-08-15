d46bdca085c46ee8af4de10aa5a20f97dd00adbd
obule <izuagbalapromise@gmail.com>
2024-08-15 15:22:37 +0200

    Fixed base url

--- src/api/requestMethods.ts
+++ src/api/requestMethods.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.BASE_URL;
+// const BASE_URL = import.meta.env.BASE_URL;
+const BASE_URL = `https://dev-api.verde24health.com`;
 const data = localStorage.getItem("persist:root");
 const user = data ? JSON.parse(data)?.user : null;
 const currentUser = JSON.parse(user)?.currentUser;
 const TOKEN = currentUser?.token;
 export const publicRequest = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
