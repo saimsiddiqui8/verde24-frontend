77af6904c04c3187cd610103643f1e7cfdd0a60c
wahaj248 <wahajkhan2488@gmail.com>
2024-09-01 02:15:15 +0500

    updatetoday

--- src/pages/DoctorPages/doctorDashboard/doctorInputInfo/consultationForm/queries.ts
+++ src/pages/DoctorPages/doctorDashboard/doctorInputInfo/consultationForm/queries.ts
@@ -68,39 +68,3 @@ query FindDoctorById($findDoctorByIdId: Int!) {
   }
 }
 `;
-
-export const GET_DOCTOR_QUERY = `
-query FindDoctorById($findDoctorByIdId: Int!) {
-  findDoctorById(id: $findDoctorByIdId) {
-    id
-    online
-    first_name
-    last_name
-    email
-    phone_number
-    gender
-    password
-    is_verified
-    verification_code
-    verification_code_expiry
-    image
-    city
-    country
-    department
-    experience
-    registration_no
-    qualification
-    consultation_mode
-    consultation_fee_regular
-    consultation_fee_discounted
-    booking_lead_time
-    payout_method
-    payout_method_id
-    address
-    postal_code
-    services
-    specialization
-    bibliography
-  }
-}
-`;
